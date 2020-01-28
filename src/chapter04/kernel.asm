3257f2752e971245d38643d37c8c3f995652ff4b
srdczk <srdyjj@163.com>
2020-01-28 11:20:37 +0800

    protected mode

---
+++ src/chapter04/kernel.asm
@@ -0,0 +1,25 @@
+org 0x8000
+
+entry:
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov si, msg
+
+putloop:
+    mov al, [si]
+    add si, 1
+    cmp al, 0
+    je fin
+    mov ah, 0x0e
+    mov bx, 15
+    int 0x10
+    jmp putloop
+
+fin:
+    HLT
+    jmp fin
+
+msg:
+    db "zenmeshuone giooo"
---
+++ src/chapter05/GDT.java
@@ -0,0 +1,8 @@
+package chapter05;
+
+public class GDT {
+    // GDT 结构
+    // 一共 8 个字节
+    // \0\1\2\3\4\5\6\7\
+    
+}
